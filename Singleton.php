ce3debe1ea48422184c20f9164decba502ab7336
darkfriend <maslov.tlt@gmail.com>
2019-12-25 00:17:33 +0300

    begin singleton trait

---
+++ Singleton.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace darkfriend\helpers;
+
+
+/**
+ * Trait Singleton
+ * @package darkfriend\helpers
+ * @author darkfriend <hi@darkfriend.ru>
+ * @version 1.0.0
+ */
+trait Singleton
+{
+    /** @var self */
+    private static $_instance;
+
+    /**
+     * Singleton
+     * @param bool $newSession create new instance
+     * @param array $options
+     * @return self
+     * @throws \Exception
+     */
+    public static function getInstance($newSession = false, $options = [])
+    {
+        if (!function_exists('curl_init')) {
+            throw new \Exception('Curl is not found!');
+        }
+        if (!self::$_instance || $newSession) {
+            self::$_instance = new self($options);
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Constructor
+     * @param array $options
+     */
+    protected function __construct($options = [])
+    {}
+}
